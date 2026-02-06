c371f0cbbebf53b5428eb4b1e965b9da3412da5b
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2026-02-07 02:05:58 +0700

    feat: enable CORS with specified origins and methods.

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,22 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://main-story-test.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Library Management API')
@@ -15,6 +29,7 @@ async function bootstrap() {
     .setVersion('1.3')
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
